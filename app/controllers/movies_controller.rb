f59b485d50c0e81dba2bb3576609ee9dc32f9411
Geoffroy <geoffroyseive@gmail.com>
2014-05-29 13:31:40 -0300

    hw 2 part 2 initial work with checkboxes

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,7 @@
 class MoviesController < ApplicationController
 
+ 
+
   def show
     id = params[:id] # retrieve movie ID from URI route
     @movie = Movie.find(id) # look up movie by unique ID
@@ -7,6 +9,7 @@ class MoviesController < ApplicationController
   end
 
   def index
+     @all_ratings = Movie.movie_ratings
     if params.key?(:sort)
       @movies = Movie.order(params[:sort]).all
       
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,8 @@
 class Movie < ActiveRecord::Base
   attr_accessible :title, :rating, :description, :release_date
+
+  def self.movie_ratings
+  	return ['G', 'PG', 'PG-13', 'R']
+  end
+
 end
